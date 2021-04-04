040a76d428194069c595dd4ccb641ee30eae4592
kunal kakade <kunalkakade808@gmail.com>
2021-04-04 19:17:19 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App title="React project with Webpack and babel from scratch"/>
+    hiiiiisss
+  </StrictMode>,
+  rootElement
+);
+
+module.hot.accept();
